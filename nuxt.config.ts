6ddb0cef725208943955fd0dd07a122970a94e48
Denys <levchenkoden02@gmail.com>
2025-01-04 23:58:23 +0200

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  target: "static",
+  ssr: true,
+  target: "server",
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   css: ["~/assets/main.css"],
