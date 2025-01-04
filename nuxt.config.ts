4f42ee6c8f635281c6f452285ef6755fa509ed0e
Denys <levchenkoden02@gmail.com>
2025-01-05 00:58:42 +0200

    Fix problems

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   css: ["~/assets/main.css"],
