ed638a9af407d5d6f4ccbe823ddaef4cee9c1f11
Denys <levchenkoden02@gmail.com>
2025-01-14 12:22:16 +0200

    Fix vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -12,9 +11,13 @@ export default defineNuxtConfig({
     },
   },
 
+  nitro: {
+    preset: "vercel"
+  },
+
   modules: ["@nuxt/image", "@nuxt/icon", "@nuxtjs/google-fonts"],
   runtimeConfig: {
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
   },
 });
